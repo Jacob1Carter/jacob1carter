12f304207869a151954120900830bac63ce218e9
Jacob1Carter <jacob1carter@outlook.com>
2026-05-30 21:42:45 +0100

    Test notifs

--- mainroutes.py
+++ mainroutes.py
@@ -5,3 +5,7 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def index():
     return render_template("main/dashboard.html", title="Dashboard")
+
+@main.route("/notif")
+def notif_demo():
+    return render_template("main/notif.html")
